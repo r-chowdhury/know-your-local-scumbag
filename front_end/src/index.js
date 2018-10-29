fc37d0fdfedde3766c26128e2508ce6e461bd7a2
Ryhan Chowdhury <ryhan.chowdhury3@gmail.com>
2018-10-29 10:47:31 -0500

    bye bye redux

--- front_end/src/index.js
+++ front_end/src/index.js
@@ -1,18 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
 
-
-
-const store = createStore(
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
-
-ReactDOM.render(
-  <Provider store={store}>
-    <App />  
-  </Provider>,
-document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
